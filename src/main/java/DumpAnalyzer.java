c4dbafd073f75d5c4ee9f1c8ec9573400331acf4
timeisenbuerger <38813829+timeisenbuerger@users.noreply.github.com>
2019-03-24 02:07:56 +0100

    First version of article collection

--- src/main/java/DumpAnalyzer.java
+++ src/main/java/DumpAnalyzer.java
@@ -1,6 +1,14 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.apache.spark.SparkConf;
 import org.apache.spark.SparkContext;
+import org.apache.spark.api.java.function.MapFunction;
 import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SQLContext;
 import org.apache.spark.sql.SparkSession;
@@ -18,8 +26,6 @@ public class DumpAnalyzer
    private static SparkContext sc;
    private static SQLContext sqlC;
 
-   private SparkConf sparkConf;
-
    static
    {
       joinedTableSchema = new StructType(new StructField[]{
@@ -31,6 +37,10 @@ public class DumpAnalyzer
             DataTypes.createStructField("cl_type", DataTypes.StringType, true)
       });
    }
+
+   //Page id von Animals: 21143238
+
+   private SparkConf sparkConf;
 
    public DumpAnalyzer()
    {
@@ -47,8 +57,59 @@ public class DumpAnalyzer
    public void analyzeArticlesRelatedToAnimals()
    {
       Dataset<Row> joinedCsv = sqlC.read().schema(joinedTableSchema).csv(CSV_DIRECTORY + "page_categorylinks_joined.csv");
+      joinedCsv.cache();
+      Dataset<Row> animals = joinedCsv.where("cl_to = 'Animals'");
 
-      Dataset<Row> animals = joinedCsv.where("cl_from = 'Animals'");
-      animals.show();
+      List<String> resultList = animals.map((MapFunction<Row, String>) entry -> entry.mkString(";"), Encoders.STRING()).collectAsList();
+
+      Map<Integer, List<String>> levelArticlesMap = new HashMap<>();
+      int level = 0;
+
+      levelArticlesMap = collectLevelArticlesMap(joinedCsv, resultList, levelArticlesMap, level);
+
+      System.out.println(levelArticlesMap);
+   }
+
+   private Map<Integer, List<String>> collectLevelArticlesMap(Dataset<Row> joinedCsv, List<String> resultList, Map<Integer, List<String>> levelArticlesMap, int level)
+   {
+      for( String row : resultList )
+      {
+         List<String> columnValues = Arrays.asList(row.split(";"));
+         String page_title = columnValues.get(2);
+         String cl_to = columnValues.get(4);
+         String cl_type = columnValues.get(5);
+
+         if( cl_type.equals("page") )
+         {
+            List<String> articlesForLevel = levelArticlesMap.get(level);
+            if( articlesForLevel == null )
+            {
+               articlesForLevel = new ArrayList<>();
+            }
+            articlesForLevel.add(page_title);
+
+            levelArticlesMap.put(level, articlesForLevel);
+         }
+         else if( cl_type.equals("subcat") )
+         {
+            Dataset<Row> resultSet = joinedCsv.where("cl_to = '" + cl_to + "'");
+            List<String> list = resultSet.map((MapFunction<Row, String>) entry -> entry.mkString(";"), Encoders.STRING()).collectAsList();
+
+            levelArticlesMap = collectLevelArticlesMap(joinedCsv, list, levelArticlesMap, level + 1);
+         }
+         else if( cl_type.equals("null") )
+         {
+            List<String> articlesForLevel = levelArticlesMap.get(level);
+            if( articlesForLevel == null )
+            {
+               articlesForLevel = new ArrayList<>();
+            }
+            articlesForLevel.add(page_title);
+
+            levelArticlesMap.put(-1, articlesForLevel);
+         }
+      }
+
+      return levelArticlesMap;
    }
 }
